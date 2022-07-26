3c18c91d7e788f0d1aa408b4813dcb88c4087944
anerishah1712 <109936486+anerishah1712@users.noreply.github.com>
2022-07-26 18:38:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const quizData = [
+    {
+        question: " Which is the world’s rarest big cat?",
+        a: "Snow leopard",
+        b: "Amur leopard",
+        c: "Tiger",
+        correct: "b",
+    };
+    {
+        question: " Beagles belong to which group of dogs?",
+        a: "Hounds",
+        b: "Retrievers",
+        c: "Pointers",
+        correct: "a",
+    };
+    {
+        question: " The Chihuahua breed is native to which country?",
+        a: "Mexico",
+        b: "France",
+        c: "Bolivia",
+        correct: "a",
+    };
+    {
+        question: " How many whiskers does the average cat have on each side of its face?",
+        a: "12",
+        b: "3",
+        c: "1",
+        correct: "a",
+    };
+    {
+        question: " Pugs used to be ruled by?",
+        a: "Egyptian pharaohs",
+        b: "Roman emperors",
+        c: "Chinese emperors",
+        correct: "b",
+    };
+];
+const quiz= document.getElementById('quiz')
+const answerE1s = document.querySelectorAll('answer')
+const questionE1 = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+
